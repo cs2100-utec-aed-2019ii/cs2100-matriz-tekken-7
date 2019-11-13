9bd36b5a629ccb9767e66e0afe1c5c683412f128
BHN16 <34187868+BHN16@users.noreply.github.com>
2019-11-13 15:50:54 -0500

    feature<Matrix.h> :  Correct insertion of values ​​and sum of matrices added.

--- Matrix.h
+++ Matrix.h
@@ -28,43 +28,63 @@ public:
        else {
             IndexNodeC<T>** column = &F.root->next;
             IndexNodeR<T>** row = &F.root->down;
-            while((*row)) {
-                if((*row)->down) {
-                    if ((*row)->down->indice > std::get<0>(par)) {
-                        IndexNodeR<T>* temporal = (*row)->down;
-                        IndexNodeR<T>* in_index = new IndexNodeR<T>(std::get<0>(par));
-                        (*row)->down = in_index;
-                        in_index->down = temporal;
-                        row = &in_index;
+            if((*row)->indice > std::get<0>(par)) {
+                IndexNodeR<T> *in_index = new IndexNodeR<T>(std::get<0>(par));
+                in_index->down = (*row);
+                (*row) = in_index;
+            }
+            else if((*row)->indice == std::get<0>(par)) {
+
+            }
+            else {
+                while ((*row)) {
+                    if ((*row)->down) {
+                        if ((*row)->down->indice > std::get<0>(par)) {
+                            IndexNodeR<T> *temporal = (*row)->down;
+                            IndexNodeR<T> *in_index = new IndexNodeR<T>(std::get<0>(par));
+                            (*row)->down = in_index;
+                            in_index->down = temporal;
+                            row = &in_index;
+                            break;
+                        }
+                    }
+                    if ((*row)->indice == std::get<0>(par)) {
                         break;
                     }
+                    row = &(*row)->down;
                 }
-                if((*row)->indice == std::get<0>(par)) {
-                    break;
+                if (!(*row)) {
+                    (*row) = new IndexNodeR<T>(std::get<0>(par));
                 }
-                row = &(*row)->down;
             }
-            if(!(*row)) {
-                (*row) = new IndexNodeR<T>(std::get<0>(par));
+            if((*column)->indice > std::get<1>(par)) {
+                IndexNodeC<T>* in_index = new IndexNodeC<T>(std::get<1>(par));
+                in_index->next = (*column);
+                (*column) = in_index;
             }
-            while((*column)) {
-                if((*column)->next) {
-                    if ((*column)->next->indice > std::get<1>(par)) {
-                        IndexNodeC<T>* temporal = (*column)->next;
-                        IndexNodeC<T>* in_index = new IndexNodeC<T>(std::get<1>(par));
-                        (*column)->next = in_index;
-                        in_index->next = temporal;
-                        column = &in_index;
+            else if((*column)->indice == std::get<1>(par)){
+
+            }
+            else {
+                while ((*column)) {
+                    if ((*column)->next) {
+                        if ((*column)->next->indice > std::get<1>(par)) {
+                            IndexNodeC<T> *temporal = (*column)->next;
+                            IndexNodeC<T> *in_index = new IndexNodeC<T>(std::get<1>(par));
+                            (*column)->next = in_index;
+                            in_index->next = temporal;
+                            column = &in_index;
+                            break;
+                        }
+                    }
+                    if ((*column)->indice == std::get<1>(par)) {
                         break;
                     }
+                    column = &(*column)->next;
                 }
-                if((*column)->indice == std::get<1>(par)) {
-                    break;
+                if (!(*column)) {
+                    (*column) = new IndexNodeC<T>(std::get<1>(par));
                 }
-                column = &(*column)->next;
-            }
-            if(!(*column)) {
-                (*column) = new IndexNodeC<T>(std::get<1>(par));
             }
 
             ValNode<T>** row2 = &(*row)->next;
@@ -106,18 +126,8 @@ public:
             IndexNodeR<T>* r = p.root->down;
             IndexNodeC<T>* c = p.root->next;
             ValNode<T>* data = r->next;
-            int indexc = 0;
-            int indexr = 0;
-            while(r) {
-                indexr = r->indice;
-                r = r->down;
-            }
-            while(c) {
-                indexc = c->indice;
-                c = c->next;
-            }
-            r = p.root->down;
-            c = p.root->next;
+            int indexc = p.get_dimensiones().second;
+            int indexr = p.get_dimensiones().first;
             for(int i = 0; i < indexr+1; i++) {
                 for(int j = 0; j < indexc+1; j++) {
                     if(r->indice != i) {
@@ -145,6 +155,33 @@ public:
                 }
                 os << std::endl;
             }
+        }
+        return os;
+    }
+
+    std::pair<int,int> get_dimensiones() {
+        IndexNodeR<T>* a = root->down;
+        IndexNodeC<T>* b = root->next;
+        int row = 0;
+        int col = 0;
+        std::pair<int,int> dimensiones(0,0);
+
+        if(!a) {
+            std::cout << "No hay valores en la matriz.\n";
+            return dimensiones;
+        }
+        else {
+            while(a) {
+                row = a->indice;
+                a = a->down;
+            }
+            while(b) {
+                col = b->indice;
+                b = b->next;
+            }
+            dimensiones.first = row;
+            dimensiones.second = col;
+            return dimensiones;
         }
     }
 
@@ -196,5 +233,108 @@ public:
     std::ostream& operator<<(std::ofstream& jj) {
 
     }
+
+    void operator = (Matrix<T> F) {
+        root = F.root;
+    }
+
+    Matrix<T> operator+(Matrix<T>& A) {
+        Matrix<T> c;
+        int rowa = A.get_dimensiones().first;
+        int columna = A.get_dimensiones().second;
+        int rowb = get_dimensiones().first;
+        int columnb = get_dimensiones().second;
+        if(rowa == rowb && columna == columnb) {
+            IndexNodeR<T>* a = A.root->down;
+            IndexNodeR<T>* b = root->down;
+            ValNode<T>* vala = a->next;
+            ValNode<T>* valb = b->next;
+            int decision = 0;
+
+            for(int i = 0; i < rowa+1; i++) {
+                for(int j = 0; j < columna+1; j++) {
+                    if(a->indice != i && b->indice != i) {
+                        decision = 0;
+                        break;
+                    }
+                    else if(a->indice == i && b->indice != i) {
+                        decision = 1;
+                        if(vala) {
+                            if(vala->x == j) {
+                                c << std::make_tuple(vala->y,vala->x,vala->value);
+                                vala = vala->next;
+                            }
+                        }
+                    }
+                    else if(b->indice == i && a->indice != i) {
+                        decision = 2;
+                        if(valb) {
+                            if(valb->x == j) {
+                                c << std::make_tuple(valb->y,valb->x,valb->value);
+                                valb = valb->next;
+                            }
+                        }
+                    }
+                    else if(a->indice == i && b->indice == i) {
+                        decision = 3;
+                        if(vala || valb) {
+                            if(vala && !valb) {
+                                c << std::make_tuple(vala->y,vala->x,vala->value);
+                                vala = vala->next;
+                            }
+                            else if(valb && !vala) {
+                                c << std::make_tuple(valb->y,valb->x,valb->value);
+                                valb = valb->next;
+                            }
+                            else if(!vala && !valb) {
+                                break;
+                            }
+                            else if(valb->x < vala->x) {
+                                c << std::make_tuple(valb->y,vala->x,valb->value);
+                                valb = valb->next;
+                            }
+                            else if(vala->x < valb->x) {
+                                c << std::make_tuple(valb->y,vala->x,vala->value);
+                                vala = vala->next;
+                            }
+                            else if(vala->x == valb->x) {
+                                T suma = vala->value + valb->value;
+                                c << std::make_tuple(vala->y,vala->x, suma);
+                                vala = vala->next;
+                                valb = valb->next;
+                            }
+                        }
+                    }
+                }
+                if(decision == 0){
+
+                }
+                else if(decision == 1) {
+                    a = a->down;
+                    if(!a) {
+                        break;
+                    }
+                    vala = a->next;
+                }
+                else if(decision == 2) {
+                    b = b->down;
+                    valb = b->next;
+                }
+                else if(decision == 3) {
+                    a = a->down;
+                    b = b->down;
+                    if(!a) {
+                        break;
+                    }
+                    vala = a->next;
+                    valb = b->next;
+                }
+            }
+            return c;
+        }
+        else {
+            std::cout << "Estas matrices no se pueden sumar.\n";
+        }
+    }
 };
-#endif 
+#endif
